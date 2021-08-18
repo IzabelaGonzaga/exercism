7e0700922c1bbdcb23ee745e807eaa9e231458a1
izabela.silva <izabela.silva@ambevtech.com.br>
2021-08-18 15:27:30 -0300

    javascript ex 1 - hello world

---
+++ teams/start-tech/javascript/hello-world/hello-world.js
@@ -0,0 +1,7 @@
+//
+// Izabela's "Hello World' exercise
+//
+
+export const hello = () => {             
+  return ("Hello, World!");      //utilizei o return que fecha a função e devolve o resultado
+}
