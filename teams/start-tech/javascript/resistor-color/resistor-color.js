94b9b888157c771a073ef483fc05819709bc9ef0
IzabelaGonzaga <izabela.silva@ambevtech.com.br>
2021-08-24 14:24:12 -0300

    javascript - ex 9 - resistor color

---
+++ teams/start-tech/javascript/resistor-color/resistor-color.js
@@ -0,0 +1,9 @@
+//
+// This is the 'Resistor Color' exercise. 
+//
+
+export const colorCode = (band) => {
+  return COLORS.indexOf(band);  // retorna o valor do índice da posição que está band no array COLORS
+};
+
+export const COLORS = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];  // declaração do array e inserção de itens
