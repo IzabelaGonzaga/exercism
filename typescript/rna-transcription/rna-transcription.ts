21eca9283e6b7eeaac1dac137ae8e8a900a88c92
IzabelaGonzaga <izabela.silva@ambevtech.com.br>
2021-10-04 19:38:20 -0300

    typescript - ex 6 - rna transcription

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,18 +1,18 @@
-enum DnaBasis {  //conjunto de elementos pré-definidos para uso na função toRna()
+enum dnaBasis {  //conjunto de elementos pré-definidos para uso na função toRna()
   G = 'C',
   C = 'G',
   T = 'A',
   A = 'U',
 }
 
-type DnaBase = keyof typeof DnaBasis;  //define DnaBase como um tipo de chave de DnaBasis
+type dnaBase = keyof typeof dnaBasis;  //define dnaBase como um tipo de chave de dnaBasis
 
 export function toRna(dna: string): string {  //função recebe uma string de nome dna e retorna uma string
   let rna: string = "";  //declara uma string chamada rna vazia que armazenará o resultado da tradução do dna
 
   for (let elemento of dna) {  //laço for que dependerá do tamanho da string de entrada e permitirá a leitura de um elemento da string por vez
-    if(!(elemento in DnaBasis)) throw Error('Invalid input DNA.');  //verificar se a chave em questão pertence a DnaBasis (operador in), caso não retorna um erro e encerra a função
-    rna += DnaBasis[<DnaBase>elemento];  //string rna recebe o elemento equivalente a chave elemento, do tipo DnaBase, contido em DnaBasis 
+    if(!(elemento in dnaBasis)) throw Error('Invalid input DNA.');  //verificar se a chave em questão pertence a dnaBasis (operador in), caso não retorna um erro e encerra a função
+    rna += dnaBasis[<dnaBase>elemento];  //string rna recebe o elemento equivalente a chave elemento, do tipo dnaBase, contido em dnaBasis 
   }
 
   return rna;  //ao fim da leitura da string dna e tradução, caso todos os elementos tenham sido traduzidos, retorna a string rna obtida
